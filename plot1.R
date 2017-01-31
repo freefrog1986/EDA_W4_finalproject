b5c7178611fbe3d350003bb6dca2f250d9d8c2dd
freefrog1986 <baihu1111@126.com>
2017-01-31 15:52:40 +0800

    download and read files

---
+++ plot1.R
@@ -0,0 +1,11 @@
+#### set working dir
+setwd('/Users/freefrog/Studing/DataScience/gitrepo/EDA_W4_finalproject')
+rm(list = ls())
+
+#### download and read files
+url <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip'
+localfile <- file.path(getwd(),'original_file')
+download.file(url, localfile)
+unzip(localfile)
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
