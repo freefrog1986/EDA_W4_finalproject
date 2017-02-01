f890353d02d1e7bafe2a87ab2f3d34c6efc9bea4
freefrog1986 <baihu1111@126.com>
2017-02-01 21:22:00 +0800

    update plot3

--- plot3.R
+++ plot3.R
@@ -20,6 +20,8 @@ submary<- subset(NEI, subset = (fips == "24510"), c(type,Emissions,year))
 #### plot and save to file
 png(filename = "plot3.png", width = 480, height = 480)
 g<-ggplot(submary,aes(x= year, y= Emissions, color = factor(type)))
-g+geom_smooth(size = 1, linetype = 5,  method = "loess", se = FALSE)+labs(title = "Trend of different source type in the Baltimore City")
+g<-g+geom_smooth(size = 1, linetype = 5,  method = "loess", se = FALSE)
+g<-g+labs(title = "Trend of different source type in the Baltimore City")
+g+xlab("Year")+ylab("PM2.5 Total Emissions(tons)")
 dev.off()
 
