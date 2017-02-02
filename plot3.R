2c7b2f57cc82bf465937a0d7f4bd86282e1fb891
freefrog1986 <baihu1111@126.com>
2017-02-02 10:38:07 +0800

    update plot3

--- plot3.R
+++ plot3.R
@@ -16,12 +16,14 @@ SCC <- readRDS("Source_Classification_Code.rds")
 
 #### subset data in Maryland
 submary<- subset(NEI, subset = (fips == "24510"), c(type,Emissions,year))
+aggdata <- aggregate(Emissions ~ year + type, submary, sum)
 
 #### plot and save to file
 png(filename = "plot3.png", width = 480, height = 480)
-g<-ggplot(submary,aes(x= year, y= Emissions, color = factor(type)))
-g<-g+geom_smooth(size = 1, linetype = 5,  method = "loess", se = FALSE)
-g<-g+labs(title = "Trend of different source type in the Baltimore City")
-g+xlab("Year")+ylab("PM2.5 Total Emissions(tons)")
+g<-ggplot(aggdata,aes(x= year, y= Emissions, color = factor(type)))
+g + geom_line() + 
+  xlab("Year")+
+  ylab("PM2.5 Total Emissions(tons)")+
+  ggtitle("Trend of different source type in the Baltimore City")
 dev.off()
 
